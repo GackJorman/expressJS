d216b323e6bf3ce26663ac1b361b8ae922bb7700
Jack Gorman <270101881@yoobeestudent.ac.nz>
2022-08-09 15:37:44 +1200

    Update users.js
    simplified router get put delete

--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,28 @@ router.post("/new", (req, res) => {
     res.send("Create New User")
 })
 
-router.get("/:id", (req, res) => {
-    res.send(`Get specific user with ID ${req.params.id}`)
-})
+// router.get("/:id", (req, res) => {
+//     res.send(`Get specific user with ID ${req.params.id}`)
+// })
 
-router.put("/:id", (req, res) => {
-    res.send(`Update specific user with ID ${req.params.id}`)
-})
-router.delete("/:id", (req, res) => {
-    res.send(`Delete specific user with ID ${req.params.id}`)
-})
+// router.put("/:id", (req, res) => {
+//     res.send(`Update specific user with ID ${req.params.id}`)
+// })
+// router.delete("/:id", (req, res) => {
+//     res.send(`Delete specific user with ID ${req.params.id}`)
+// })
+
+// Simplified of above so no repeat code
+router
+    .route("/:id")
+    .get((req, res) => {
+        res.send(`Get specific user with ID ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`Update specific user with ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete specific user with ID ${req.params.id}`)
+    })
 
 module.exports = router
